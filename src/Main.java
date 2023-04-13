f5c552d12b2f3a3febc8da2ebc28197c3890a0bb
Karina <kulinicheva_91@mail.ru>
2023-04-13 22:35:41 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int money = 100;//начальный счет клиента
-        int x = 2000;//сумма пополнения
+        int balance = 100;//начальный счет клиента
+        int money = 2000;//сумма пополнения
         int bonus = 100;//бонус за каждые 100 рублей
-        if (x >= 1000) {
-            System.out.println(x / bonus + money + x);
-        }
-        if (x < 1000) {
-            System.out.println(x + money);
-        }
 
-
+        if (money >= 1000) {
+            System.out.println(money / bonus + balance + money);
+        } else {
+            System.out.println(money + balance);
+        }
     }
 }

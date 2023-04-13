f7f2cf4882f22d897f0d2f0e12f26a1d388d8f33
Karina <kulinicheva_91@mail.ru>
2023-04-13 23:39:46 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;//начальный счет клиента
-        int money = 2000;//сумма пополнения
+        int money = 1100;//сумма пополнения
         int bonus = 100;//бонус за каждые 100 рублей
 
-        if (money >= 1000) {
+        if (money > 1000) {
             System.out.println(money / bonus + balance + money);
         } else {
             System.out.println(money + balance);
